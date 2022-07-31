fdc4e6a0564b19875b6f60963ecb541bbc2dd214
SyahmiEdri <s211501501@studentmail.unimap.edu.my>
2022-07-31 16:46:37 +0800

    Update main.py

--- Unimap Complain System/main.py
+++ Unimap Complain System/main.py
@@ -11,7 +11,9 @@ root.title('Unimap Complain System')
 root.geometry('1920x1080+0+0')
 root.config(bg='#2c3e50')
 root.state('zoomed')
-#Put your image file location in (r'') to run
+
+#Image
+'''#Put your image file location in (r'') to run
 root.iconbitmap(r'C:\\Users\\SYAHMI EDRI\\Desktop\\Unimap Complain System\\unimap.ico')
 #Put your image file location in (Image.open()) to run
 img1=ImageTk.PhotoImage(Image.open('C:\\Users\\SYAHMI EDRI\\Desktop\\Unimap Complain System\\unimap.jpg'))
@@ -20,7 +22,7 @@ imgdisplay.pack(side=RIGHT)
 #Put your image file location in (Image.open()) to run
 img2=ImageTk.PhotoImage(Image.open('C:\\Users\\SYAHMI EDRI\\Desktop\\Unimap Complain System\\unimap.jpg'))
 imgdisplay=Label(root,image=img2)
-imgdisplay.pack(side=LEFT)
+imgdisplay.pack(side=LEFT)'''
 
 #Variable
 name=StringVar()
